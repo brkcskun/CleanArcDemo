b56f5c268dc88eddeec328c0b114f4a540d31557
brkcskun <burakc34@gmail.com>
2021-03-16 16:33:56 +0300

    Initial commit

---
+++ CleanArcDemo/CleanArcDemo.Mvc/Data/Migrations/20210316131956_Initial_Create.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArcDemo.Mvc.Data.Migrations
+{
+    public partial class Initial_Create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
